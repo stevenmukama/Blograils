649a511cb1bdaf7e5f7edef260fe9e07445c0dcb
Mukama Steven <stevenmukama1@gmail.com>
2023-04-08 11:12:17 +0200

    add requested changes

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_07_191312) do
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
-  create_table "user", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.text "bio"
     t.string "photo"
@@ -53,16 +53,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_07_191312) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.text "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
-  add_foreign_key "comments", "users", column: "post_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
